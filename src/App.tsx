9239f4d596824ed9447204f7605fb6f6b527bea8
Hashib Raja <hashibraja23@gmail.com>
2023-07-27 08:35:29 +0530

    experiment about keyframes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import { motion } from "framer-motion";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  /*
+    transition:-
+    
+    */
 
-export default App
+  return (
+    <motion.div
+      className="box"
+      animate={{
+        borderRadius: ["0%", "40%", "50%"],
+        scale: [1, 1.5, 3],
+        rotate: [0, 180, 360],
+      }}
+      transition={{
+        // type: "spring",
+        // stiffness: 100,
+        // repeat: Infinity,
+        // repeatType: "mirror",
+        duration: 2.5,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5],
+        repeat: Infinity,
+        repeatType: "mirror",
+        stiffness: 100,
+        type: "spring",
+        repeatDelay: 1,
+      }}
+    ></motion.div>
+  );
+};
+
+export default App;
